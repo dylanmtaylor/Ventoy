d6169b9126a841c26646504dfa41c684bf879968
Dylan M. Taylor <dylan@dylanmtaylor.com>
2020-08-24 21:00:24 -0400

    Update build.sh

--- vtoygpt/build.sh
+++ vtoygpt/build.sh
@@ -2,18 +2,18 @@
 
 rm -f vtoytool/00/*
 
-/opt/diet64/bin/diet -Os gcc -D_FILE_OFFSET_BITS=64  vtoygpt.c crc32.c -o  vtoygpt_64
-/opt/diet32/bin/diet -Os gcc -D_FILE_OFFSET_BITS=64 -m32 vtoygpt.c crc32.c -o  vtoygpt_32
+#/opt/diet64/bin/diet -Os gcc -D_FILE_OFFSET_BITS=64  vtoygpt.c crc32.c -o  vtoygpt_64
+#/opt/diet32/bin/diet -Os gcc -D_FILE_OFFSET_BITS=64 -m32 vtoygpt.c crc32.c -o  vtoygpt_32
 
-#gcc -D_FILE_OFFSET_BITS=64 -static -Wall vtoygpt.c  -o  vtoytool_64
-#gcc -D_FILE_OFFSET_BITS=64  -Wall  -m32  vtoygpt.c  -o  vtoytool_32
+gcc -D_FILE_OFFSET_BITS=64 -static -Wall vtoygpt.c  -o  vtoytool_64
+gcc -D_FILE_OFFSET_BITS=64  -Wall  -m32  vtoygpt.c  -o  vtoytool_32
 
 if [ -e vtoygpt_64 ] && [ -e vtoygpt_32 ]; then
     echo -e '\n############### SUCCESS ###############\n'
-    mv vtoygpt_64 ../INSTALL/tool/
-    mv vtoygpt_32 ../INSTALL/tool/
+    mv vtoygpt_64 vtoygpt_32 ../INSTALL/tool/
 else
     echo -e '\n############### FAILED ################\n'
+    ls -lah
     exit 1
 fi
 
